1b47903b658a17d03b8c0772126fc69510321531
luis9cabrera <luis9cabrera@outlook.com>
2026-02-27 16:52:24 -0600

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://luis9cabrera.github.io",
+    base: "/lab",
     output: "static",
     vite: {
         plugins: [tailwindcss()]
