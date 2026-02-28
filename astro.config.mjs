5c3032b43e1aaa3859520f222125683ddb27d3f1
Luis Cabrera <luis9cabrera@outlook.com>
2026-02-28 15:54:38 -0600

    Ajuste de footer y links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import favicons from "astro-favicons";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://luis9cabrera.github.io",
-    base: "/lab",
-    output: "static",
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  site: "https://luis9cabrera.github.io",
+  base: "/lab",
+  output: "static",
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [favicons()]
 });
